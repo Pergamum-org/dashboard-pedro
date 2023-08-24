00ec0eaf733e4f64a963b2186cec611a1a5d1bf9
Romulo Pinheiro <romuloe.p.jesus@gmail.com>
2023-08-23 21:46:16 -0300

    @dashboard -v0.1: add stitches

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { globalCss } from '.'
+
+export const stylesGlobalCss = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    '-webkit-font-smoothing': 'antialiased',
+    'text-rendering': 'optimizeLegibility',
+    background: '$gray400',
+  },
+
+  'body, input, button, textarea': {
+    fontFamily: '$default',
+    fontSize: '1rem',
+    color: '$gray200',
+
+    "@media (max-width: 1000px)": {
+      fontSize: '86.75%',
+    },
+    "@media (max-width: 750px)": {
+      fontSize: '75%',
+    }
+  },
+})
